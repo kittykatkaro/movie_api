21dbb98b0ffc6d2ff6c6a79f8935e1de0842e11e
Karo <karoogorek@gmail.com>
2024-09-18 16:42:35 +0200

    Add authentication endpoint for user login

---
+++ auth.js
@@ -0,0 +1,39 @@
+const jstSecret = 'your_jwt_secret'; // same key as used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+	passport = require('passport');
+
+require('./passport'); //local passport file
+
+let generateJWTToken = (user) => {
+	return jwt.sign(user, jstSecret, {
+		subject: user.username, //this is the username you're encoding in the JWT
+		expiresIn: '7d', //this specifies that the token will expire in 7 days
+		algorithm: 'HS256', //this is the algorithm used to 'sign' or encode the values of the JWT
+	});
+};
+
+// POST login
+module.exports = (router) => {
+	router.post('/login', (req, res) => {
+		passport.authenticate(
+			'local',
+			{ session: false },
+			(error, user, info) => {
+				if (error || !user) {
+					return res.status(400).json({
+						message: 'Something is not right',
+						user: user,
+					});
+				}
+				req.login(user, { session: false }, (error) => {
+					if (error) {
+						res.send(error);
+					}
+					let token = generateJWTToken(user.toJSON());
+					return res.json({ user, token });
+				});
+			}
+		)(req, res);
+	});
+};
