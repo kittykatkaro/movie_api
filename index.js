cceb36e9faa4a41ab009a634b3da82f0ef8b3fb3
Karo <karoogorek@gmail.com>
2024-09-20 13:59:01 +0200

    Refactor variable name for hashed password in user registration

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.post(
 				} else {
 					Users.create({
 						username: req.body.username,
-						password: req.body.password,
+						password: hashedPassword,
 						email: req.body.email,
 						birthday: req.body.birthday,
 					})
