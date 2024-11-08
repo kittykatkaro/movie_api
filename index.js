ba44cf233cba00bf682a5ce0bfb23d030ff1b267
Karo <karoogorek@gmail.com>
2024-11-08 11:08:43 +0100

    Simplify CORS configuration by removing custom origin handling

--- index.js
+++ index.js
@@ -32,18 +32,19 @@ let allowedOrigins = [
 	'https://karoflix.netlify.app/',
 ];
 app.use(
-	cors({
-		origin: (origin, callback) => {
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				let message =
-					'The CORS policy for this application doesn’t allow access from origin ' +
-					origin;
-				return callback(new Error(message), false);
-			}
-			return callback(null, true);
-		},
-	})
+	cors()
+	// 	{
+	// 	origin: (origin, callback) => {
+	// 		if (!origin) return callback(null, true);
+	// 		if (allowedOrigins.indexOf(origin) === -1) {
+	// 			let message =
+	// 				'The CORS policy for this application doesn’t allow access from origin ' +
+	// 				origin;
+	// 			return callback(new Error(message), false);
+	// 		}
+	// 		return callback(null, true);
+	// 	},
+	// }
 );
 
 let auth = require('./auth')(app);
